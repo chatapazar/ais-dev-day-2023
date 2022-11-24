c3dc9e404571d3b277f8abd6f5c3bff6a48114a1
Audomsak Suksunti <s.audomsak@gmail.com>
2022-11-24 11:02:27 +0700

    updated lab user provisioner to install Grafana operator

--- script/lab-user-provisioner.sh
+++ script/lab-user-provisioner.sh
@@ -3,6 +3,13 @@
 ####################################################
 # Functions
 ####################################################
+repeat() {
+    echo
+    for i in {1..120}; do
+        echo -n "$1"
+    done
+    echo
+}
 
 provision_devspaces_project_in_advance() {
     oc login -u opentlc-mgr -p $ADMIN_PASSWORD --insecure-skip-tls-verify
@@ -12,6 +19,7 @@ provision_devspaces_project_in_advance() {
         oc label namespace $project app.kubernetes.io/part-of=che.eclipse.org
         oc label namespace $project app.kubernetes.io/component=workspaces-namespace
         oc annotate namespace $project che.eclipse.org/username=$user
+        repeat '-'
     done
 }
 
@@ -20,6 +28,52 @@ create_projects() {
         oc login -u user$i -p $USER_PASSWORD --insecure-skip-tls-verify
         oc new-project user$i-devspaces
         oc new-project user$i-superheros
+        oc new-project user$i-istio-system
+        repeat '-'
+    done
+}
+
+install_operator() {
+    operatorNameParam=$1
+    operatorDescParam=$2
+    ymlFilePathParam=$3
+    project=$4
+
+    echo
+    echo "Installing $operatorDescParam..."
+    echo
+
+    oc apply -f $ymlFilePathParam -n $project
+
+    echo
+    echo "Waiting for $operatorDescParam to be available..."
+    echo
+
+    available="False"
+
+    while [[ $available != "True" ]]; do
+        sleep 5
+        available=$(oc get -n $project operators.operators.coreos.com \
+            $operatorNameParam.$project \
+            -o jsonpath='{.status.components.refs[?(@.apiVersion=="apps/v1")].conditions[?(@.type=="Available")].status}')
+    done
+
+    echo "$operatorDescParam is now available!"
+}
+
+install_grafana() {
+    oc login -u opentlc-mgr -p $ADMIN_PASSWORD --insecure-skip-tls-verify
+
+    for ((i = 1; i <= $totalUsers; i++)); do
+        project=user$i-superheros
+        sed "s/NAMESPACE/${project}/g" ../manifest/operator-group.yml | oc apply -f- -n $project
+
+        operatorName=grafana-operator
+        operatorDesc="Grafana Operator"
+        ymlFilePath=../manifest/grafana-subscription.yml
+
+        install_operator $operatorName "$operatorDesc" $ymlFilePath $project
+        repeat '-'
     done
 }
 
@@ -29,4 +83,5 @@ create_projects() {
 totalUsers=$1
 
 create_projects
-provision_devspaces_project_in_advance
+install_grafana
+#provision_devspaces_project_in_advance
