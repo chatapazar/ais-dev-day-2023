1e5db667f2a3fd294ef1c076aa3be43b400cd726
Audomsak Suksunti <s.audomsak@gmail.com>
2023-08-23 01:51:15 +0700

    add custom micrometer metric config classes

---
+++ code/event-statistics/src/main/java/io/quarkus/sample/superheroes/statistics/config/MeterConfig.java
@@ -0,0 +1,24 @@
+package io.quarkus.sample.superheroes.statistics.config;
+
+import java.util.Arrays;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.quarkus.micrometer.runtime.MeterFilterConstraint;
+import javax.inject.Singleton;
+import javax.enterprise.inject.Produces;
+
+@Singleton
+public class MeterConfig {
+
+    /** Define common tags that apply only to a Prometheus Registry */
+    @Produces
+    @Singleton
+    @MeterFilterConstraint(applyTo = PrometheusMeterRegistry.class)
+    public MeterFilter configurePrometheusRegistries() {
+        return MeterFilter.commonTags(Arrays.asList(
+                Tag.of("application", "event-statistics")));
+    }
+
+}
---
+++ code/rest-heroes/src/main/java/io/quarkus/sample/superheroes/hero/config/MeterConfig.java
@@ -0,0 +1,24 @@
+package io.quarkus.sample.superheroes.hero.config;
+
+import java.util.Arrays;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.quarkus.micrometer.runtime.MeterFilterConstraint;
+import javax.inject.Singleton;
+import javax.enterprise.inject.Produces;
+
+@Singleton
+public class MeterConfig {
+
+    /** Define common tags that apply only to a Prometheus Registry */
+    @Produces
+    @Singleton
+    @MeterFilterConstraint(applyTo = PrometheusMeterRegistry.class)
+    public MeterFilter configurePrometheusRegistries() {
+        return MeterFilter.commonTags(Arrays.asList(
+                Tag.of("application", "rest-heroes")));
+    }
+
+}
